12325b1c9d2f0466a4ac182805e7cd67cc92bd20
sayed-1625 <sandraleonorhidalgoortiz@gmail.com>
2024-08-21 05:34:08 -0500

    Corregir imagen

--- server/settings.py
+++ server/settings.py
@@ -131,7 +131,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
 #1 render static
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
